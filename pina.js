d0b859e1193f713c24c741cc432c526b4e9c1654
Elias Roussos <eliasIsaiah@outlook.com>
2020-04-07 21:22:10 -0400

    first commit

---
+++ pina.js
@@ -0,0 +1,22 @@
+var likesPinas;
+var likesGettingCaughtInTheRain;
+var isNotIntoYoga;
+var hasHalfABrain;
+var likesMakingLoveAtMidnightOnTheDunesByTheCape;
+
+likesPinas = confirm("do you like pina coladas");
+likesGettingCaughtInTheRain = confirm("and getting caught in the rain");
+isNotIntoYoga = confirm("are you not into yoga");
+hasHalfABrain = confirm("do you have half a brain");
+likesMakingLoveAtMidnightOnTheDunesByTheCape = confirm("do you like making love at midnight on the dunes by the cape");
+
+if (likesPinas &&
+    likesGettingCaughtInTheRain &&
+    isNotIntoYoga &&
+    hasHalfABrain &&
+    likesMakingLoveAtMidnightOnTheDunesByTheCape) {
+	alert("I'm the one that you've looked for - come with me and escape")
+}
+else {
+	alert("thank u, next!")
+}
